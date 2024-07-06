56d42d1b7394e227af36eafd0f8f1652fdaa4073
NxNE <NxNE2024@gmail.com>
2024-07-06 01:11:51 -0700

    Add history feature and update migrations

---
+++ manage.py
@@ -0,0 +1,9 @@
+from flask_migrate import Migrate
+from flask.cli import FlaskGroup
+from app import app, db
+
+migrate = Migrate(app, db)
+cli = FlaskGroup(app)
+
+if __name__ == '__main__':
+    cli()
