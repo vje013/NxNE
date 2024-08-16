f594244c67fbf1563cbad36901dcbf5c08b19898
NxNE <NxNE2024@gmail.com>
2024-08-16 12:01:27 -0700

    Changed home

---
+++ user_system/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Vladimir Edouard">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device=width, initial-scale=1, shrink-to-fit=no">
+    <title>NxNE</title>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <!--Login Form Start -->
+        <div class="row justify-content-center my-auto">
+            <div class="col-lg-10">
+                <div class="card-group">
+                </div>
+            </div>    
+        </div>   
+        <!-- Login Form End -->
+    </div>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"></script>
+</body>
+</html>
